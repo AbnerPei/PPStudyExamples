26d295736076ba3e4d0d7d49a0414243b60227a8
PeiJianbo <peijb@exdl.cn>
2020-06-30 09:00:36 +0800

    klpicker重构

---
+++ OnceCoderForeverCoder/MyLibs/KLPickerExample/KLPickerExample/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  KLPickerExample
+//
+//  Created by PPAbner on 2020/6/29.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
